93e92b1e212b3671ed678f5d932cf5f7a7588b27
ArnoldMsl <arnold.masselin@gmail.com>
2024-04-04 16:32:08 +0200

    CSS responsive design OK

--- assets/scripts/circle.js
+++ assets/scripts/circle.js
@@ -1,11 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     const segments = document.getElementsByClassName('circle-segment');
-    console.log(segments.length);
+    const texts = document.getElementsByClassName('circle-segment-text');
 
-    function positionSegment(element, theta, i) {
-        element.style.left = 50 + (44.15 * Math.cos(theta)) + '%';
-        element.style.top = 50 + (44.15 * Math.sin(theta)) + '%';
+
+    function positionSegment(element, angle, i) {
+        element.style.left = 50 + (44.15 * Math.cos(angle)) + '%';
+        element.style.top = 50 + (44.15 * Math.sin(angle)) + '%';
         element.style.transform = `translate(-50%, -50%) rotate(${i * 44.9}deg)`;
+    }
+
+    function positionText(text, i) {
+        text.style.transform = `rotate(${i * -45}deg)`;
     }
 
     function backgroundSegment(element, i) {
@@ -13,9 +18,10 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     for (let i = 0; i < segments.length; i++) {
-        let theta = (((Math.PI * 2) / segments.length) * i + 22.46);
-        console.log(theta);
-        positionSegment(segments[i], theta, i);
+        let angle = (((Math.PI * 2) / segments.length) * i + 22.46);
+        console.log(texts.length);
+        positionSegment(segments[i], angle, i);
+        positionText(texts[i], i);
         backgroundSegment(segments[i], i);
     }
 });
